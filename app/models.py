ccf9f7d850a5acc559e824b817df02e329650887
felkiriinya <felkiriinya@gmail.com>
2020-10-24 16:38:24 +0300

    registration of new users

--- app/models.py
+++ app/models.py
@@ -1,11 +1,20 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
     pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
     pass_secure = db.Column(db.String(255))
 
