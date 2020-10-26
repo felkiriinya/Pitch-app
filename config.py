7aec131cb081fa474636dbb91d393feb9a86c8ee
felkiriinya <felkiriinya@gmail.com>
2020-10-27 00:39:28 +0300

    deployment

--- config.py
+++ config.py
@@ -16,6 +16,7 @@ class Config:
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
+    
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://felista:ilovemyself@localhost/pitch'
     DEBUG = True
